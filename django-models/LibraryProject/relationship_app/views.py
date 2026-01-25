463f75d32b056769c84c16067277779aa5ed1186
kedwani <you@example.com>
2026-01-25 16:25:11 +0200

    update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import DetailView
 from .models import Book, Library
 
 
 def list_books(request):
-    books = Book.objects.select_related("author").all()
-    return render(request, "list_books.html", {"books": books})
+    books = Book.objects.all()
+    output = []
+
+    for book in books:
+        output.append(f"{book.title} by {book.author.name}")
+
+    return HttpResponse("<br>".join(output))
 
 
 class LibraryDetailView(DetailView):

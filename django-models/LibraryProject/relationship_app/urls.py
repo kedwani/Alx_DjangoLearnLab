aa1048d4b9e16f0cc7949d368fcee22d7f91b0b3
kedwani <you@example.com>
2026-01-25 17:44:43 +0200

    update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books
 
 # ---------- URL Patterns ----------
 # "add_book/" , "edit_book/""
